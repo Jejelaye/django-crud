0762f1bc83b8c884819ef8ddee6c1212df0fd903
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2022-06-24 14:27:14 +0100

    added template_name to blog/views.py

--- I4G011423RKS/blog/views.py
+++ I4G011423RKS/blog/views.py
@@ -8,25 +8,30 @@ from .models import Post
 # Create your views here.
 class PostListView(ListView):
   model = Post
+  template_name: str = "blog/post_list.html"
 
 
 class PostCreateView(CreateView):
   model = Post
   fields = "__all__"
   success_url = reverse_lazy("blog:all")
+  template_name: str = "blog/post_form.html"
 
 
 class PostDetailView(DetailView):
   model = Post
+  template_name: str = "blog/post_detail.html"
 
 
 class PostUpdateView(UpdateView):
   model = Post
   fields = "__all__"
   success_url = reverse_lazy("blog:all")
+  template_name: str = "blog/post_form.html"
 
 
 class PostDeleteView(DeleteView):
   model = Post
   fields = "__all__"
   success_url = reverse_lazy("blog:all")
+  template_name: str = "blog/post_form.html"
